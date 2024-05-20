caf4a36d2807f1f4c583d416db3121e8cea6088a
TaariqHoo24 <st24580@howick.school.nz>
2024-05-21 10:31:54 +1200

    Add files via upload
    Version 1 of the ticket booking system. This version allows the user to input any number of quantities of tickets and still be able to calculate a price for it, even id the total amount exceeds a quantity of 100 tickets.

---
+++ Ticket Booking System.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+class ticket_booking_system:
+    def __init__(self, master):
+        self.master = master
+        master.title('Ticket Booking System')
+
+        self.adult_price = 15
+        self.child_price = 5
+        self.student_price = 10
+
+        self.create_booking_system()
+
+    def create_booking_system(self): 
+        self.adult_label = tk.Label(self.master, text = 'Adult $15', font = ('Arial', 15))
+        self.adult_label.grid(row = 0, column = 0, pady = 10, padx = 10, sticky = 'w')
+        self.adult_entry = tk.Entry(root, font = ('Arial', 15))
+        self.adult_entry.grid(row = 0, column = 1, pady = 10, padx = 10)
+
+        self.child_label = tk.Label(self.master, text = 'Child $5', font = ('Arial', 15))
+        self.child_label.grid(row = 1, column = 0, pady = 10, padx = 10, sticky = 'w')
+        self.child_entry = tk.Entry(root, font = ('Arial', 15))
+        self.child_entry.grid(row = 1, column = 1, pady = 10, padx = 10)
+
+        self.student_label = tk.Label(self.master, text = 'Student/Senior $10', font = ('Arial', 15))
+        self.student_label.grid(row = 2, column = 0, pady = 10, padx = 10, sticky = 'w')
+        self.student_entry = tk.Entry(self.master, font = ('Arial', 15))
+        self.student_entry.grid(row = 2, column = 1, pady = 10, padx = 10)
+
+        self.total_price_button = tk.Button(self.master, text = 'Total Price:', font = ('Arial', 15, 'bold'), command = self.calc_total_price)
+        self.total_price_button.grid(row = 3, column = 0, pady = 10, padx = 10, sticky = 'w')
+        self.total_price_output_label = tk.Label(self.master, font = ('Arial', 15, 'bold'))
+        self.total_price_output_label.grid(row = 3, column = 1, pady = 10, padx = 10)
+
+    def calc_total_price(self):
+        self.adult_quantity = int(self.adult_entry.get())
+        self.child_quantity = int(self.child_entry.get())
+        self.student_quantity = int(self.student_entry.get())
+
+        total_price = (self.adult_quantity * self.adult_price) + (self.child_quantity * self.child_price) + (self.student_quantity * self.student_price)
+
+        self.total_price_output_label.config(text = f'Total Price: ${total_price}')
+
+root = tk.Tk()
+booking_system = ticket_booking_system(root)
+root.mainloop()
